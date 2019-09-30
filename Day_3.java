e13fed2b5d43e4d73c29edaa358ce17248609df0
Himanshu <jadgehimanshu1999@gmail.com>
2019-10-01 00:47:13 +0530

    Final Push For Day 3

--- Day_3.java
+++ Day_3.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Day_3 {
@@ -15,6 +16,7 @@ public class Day_3 {
 	return count;
 	}
 	
+	//Function for making all number equal
 	public static int minMoves(ArrayList<Integer> list) {
 		int min=Integer.MAX_VALUE;
 		int sum=0;
@@ -26,20 +28,56 @@ public class Day_3 {
 		}
 	return (sum)-(list.size())*(min);	
 	}
-	public static void main(String[] args) {
-	Scanner sc=new Scanner(System.in);
-    int t=sc.nextInt();
-    while(t--!=0) {
-    	int n=sc.nextInt();
-    	ArrayList<Integer> list=new ArrayList<Integer>();
-    	int min=Integer.MAX_VALUE;
-    	for(int i=0;i<n;i++) {
-    		int x=sc.nextInt();
-    		list.add(x);
-    	}
-    	System.out.println(minMoves(list));
-    }
-
+	
+	//Function for CopyPaste Question Codechef
+	public static int copyPaste(ArrayList<Integer> list) {
+		HashMap<Integer,Integer> map=new HashMap<Integer,Integer>();
+		int count=0;
+		for(int i=0;i<list.size();i++) {
+			if(map.containsKey(list.get(i))){
+				
+			}
+			else {
+				map.put(list.get(i),1);
+				count++;
+			}
+		}
+	return count;
 	}
-
+	
+	public static void main(String[] args) {
+	   
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		String str=sc.next();
+		char arr[]=new char[26];
+		for(int i=0;i<26;i++) {
+			arr[i]=str.charAt(i);
+		}
+		
+		while(t--!=0) {
+	String input=sc.next();
+	String result="";
+	for(int i=0;i<input.length();i++) {
+		if(input.charAt(i)>='a'&&input.charAt(i)<='z') {
+			int k=input.charAt(i)-'a';
+			result=result+arr[k];
+		}
+		else if(input.charAt(i)>='A'&&input.charAt(i)<='Z') {
+			int k=input.charAt(i)-'A';
+			char ch=arr[k];
+			k=ch-32;
+			ch=(char)k;
+			result=result+ch;
+		}
+		else if(input.charAt(i)=='_') {
+			result=result+" ";
+		}
+		else {
+			result+=input.charAt(i);
+		}
+	}
+		System.out.println(result);
+	}
+ }
 }
