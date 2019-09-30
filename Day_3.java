7462bdfec8ca3850434c7793e077065f183d3f0d
Himanshu <jadgehimanshu1999@gmail.com>
2019-09-30 23:32:42 +0530

    Second Push

--- Day_3.java
+++ Day_3.java
@@ -1,7 +1,9 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Day_3 {
 
+	//Function for counting all the string that start with 1 and end with 1
 	public static int countSubstring(String str) {
 		int count=0;
 		for(int i=0;i<str.length();i++) {
@@ -13,11 +15,30 @@ public class Day_3 {
 	return count;
 	}
 	
+	public static int minMoves(ArrayList<Integer> list) {
+		int min=Integer.MAX_VALUE;
+		int sum=0;
+		for(int i=0;i<list.size();i++) {
+			if(list.get(i)<min) {
+				min=list.get(i);
+			}
+		sum+=list.get(i);
+		}
+	return (sum)-(list.size())*(min);	
+	}
 	public static void main(String[] args) {
 	Scanner sc=new Scanner(System.in);
-	int n=sc.nextInt();
-	String str=sc.next();
-    System.out.println(str);		
+    int t=sc.nextInt();
+    while(t--!=0) {
+    	int n=sc.nextInt();
+    	ArrayList<Integer> list=new ArrayList<Integer>();
+    	int min=Integer.MAX_VALUE;
+    	for(int i=0;i<n;i++) {
+    		int x=sc.nextInt();
+    		list.add(x);
+    	}
+    	System.out.println(minMoves(list));
+    }
 
 	}
 
