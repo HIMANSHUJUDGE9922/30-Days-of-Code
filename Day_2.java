78086f16560dfc3b1ff5b67b7fd8de60ac45538d
Himanshu <jadgehimanshu1999@gmail.com>
2019-10-04 17:52:32 +0530

    Day 2 of Programming

--- Day_2.java
+++ Day_2.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class Day_2 {
 
 	public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+      
+		Scanner sc=new Scanner(System.in);
         int t=sc.nextInt();
         while(t--!=0) {
         	int a,b,c;
