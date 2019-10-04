de6ae8c1c4aebea91b4b9ae35f9b83d3cfa75760
Himanshu <jadgehimanshu1999@gmail.com>
2019-10-04 19:04:34 +0530

    Day 6 of Programming

--- October.java
+++ October.java
@@ -1,8 +1,13 @@
+import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.Scanner;
+
 
 public class October {
 
-	//This class contains all codes for the Octobe rLong Challenges
+	//This class contains all codes for the October Long Challenges
+	
+    //Question 1
 	public static void s10E() {
 		Scanner sc=new Scanner(System.in);
 	      int t=sc.nextInt();
@@ -32,10 +37,50 @@ public class October {
 	          System.out.println(count);
 	      }
 	}
+
+	//Question 2
+	public static void saktan() {
+		Scanner sc=new Scanner(System.in);
+		 int t=sc.nextInt();
+		 while(t--!=0) {
+			 long n=sc.nextLong();
+			 long m=sc.nextLong();
+			 long arr1[][]=new long[(int) (n+1)][(int) (m+1)];
+			 long arr2[][]=new long[(int) (n+1)][(int) (m+1)];
+			 long q=sc.nextInt();
+
+			 for(int i=0;i<q;i++){
+				 int x=sc.nextInt();
+				 int y=sc.nextInt();
+				 arr1[x][1]=arr1[x][1]+1;
+				 arr2[1][y]=arr2[1][y]+1;
+			 }
+			 for(int i=1;i<=n;i++){
+				 for(int j=1;j<=m;j++) {
+					 arr1[i][j]+=arr1[i][j-1];
+				 }
+			 }
+			 
+			 for(int i=1;i<=m;i++) {
+				 for(int j=1;j<=n;j++) {
+					 arr2[j][i]+=arr2[j-1][i];
+				 }
+			 }
+			 
+			 long count=0;
+			 for(int i=1;i<=n;i++) {
+				 for(int j=1;j<=m;j++) {
+					 arr1[i][j]=arr1[i][j]+arr2[i][j];
+					 if(arr1[i][j]%2!=0) {
+						 count++;
+					 }
+				 }
+			 }
+			System.out.println(count); 
+		 }
+	}
 	
 	public static void main(String[] args) {
-	 
 		
-		
-    }
+   }
 }
