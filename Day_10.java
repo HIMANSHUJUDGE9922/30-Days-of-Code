8ad6d25610f99129f734e9ad2dd7c20b2f361b61
Himanshu <jadgehimanshu1999@gmail.com>
2019-10-21 22:03:40 +0530

    Day 10 pf Programming

--- Day_10.java
+++ Day_10.java
@@ -141,70 +141,115 @@ public class Day_10 {
 		 }
 	
 	}
+    public static void Question_3Second() {
+    	 Scanner sc=new Scanner(System.in);
+         
+         int n=sc.nextInt();
+         int q=sc.nextInt();
+        
+         int arr[]=new int[n+1];
+         
+         for(int i=1;i<=n;i++) {
+         	arr[i]=sc.nextInt();
+         }
+ 		while(q--!=0) {
+ 			int l=sc.nextInt();
+ 			int r=sc.nextInt();
+ 			int diff=sc.nextInt();
+ 			
+ 			HashMap<Integer,Integer> map=new HashMap<Integer, Integer>();
+ 			
+ 			for(int i=l;i<=r;i++) {
+ 			    if(!map.containsKey(arr[i])){
+ 					map.put(arr[i],arr[i]);
+ 				}
+ 				
+ 			}
+ 			
+ 			for(int i=l;i<=r;i++) {
+ 				if(map.containsKey(arr[i]-diff)){
+ 					map.put(arr[i],(arr[i]-diff));
+ 				}
+ 			}
+ 			int max=1;
+ 			HashMap<Integer,Integer> map_2=new HashMap<Integer, Integer>();
+ 			for(int i=l;i<=r;i++) {
+ 				if((map.get(arr[i]))==arr[i]){
+ 					map_2.put(arr[i],1);
+ 				
+ 				}
+ 				else {
+ 					int parent=getparent(arr[i],map);
+ 					if(map_2.containsKey(parent)) {
+ 						int val=map_2.get(parent);
+ 						val=val+1;
+ 						map_2.put(parent,val);
+ 						if(val>max) {
+ 							max=val;
+ 						}
+ 					}
+ 					else {
+ 						map_2.put(parent,1);
+ 						
+ 					}
+ 				}
+ 			}
+ 			
+ 		System.out.println(max);
+ 		}
+ 		
+    }
+	
 
-	public static int getparent(int x,HashMap<Integer,Integer> map){
+    public static int getparent(int x,HashMap<Integer,Integer> map){
 		if(map.get(x)==x) {
 			return x;
 		}
 		return getparent(map.get(x),map);
 	}
-	public static void main(String[] args) {
+	
+    //Question 1 Codechef Contest
+    public static void Question_1Codechef() {
+    	 Scanner sc=new Scanner(System.in);
+         int t=sc.nextInt();
+         while(t--!=0) {
+      	   String str=sc.next();
+      	   HashMap<Character,Integer> map=new HashMap<Character, Integer>();
+      	   for(int i=0;i<str.length();i++) {
+      		   if(!map.containsKey(str.charAt(i))) {
+      			   map.put(str.charAt(i), 1);
+      			   System.out.print(str.charAt(i));
+      		   }
+      	   }
+      	   System.out.println();
+         }
+    }
+   
+    //Question 2 Codechef Contest
+    public static long modularExponentiation(long x,long n,long M)
+    {
+        if(n==0)
+            return 1;
+        else if(n%2 == 0)        //n is even
+            return modularExponentiation((x*x)%M,n/2,M);
+        else                             //n is odd
+            return (x*modularExponentiation((x*x)%M,(n-1)/2,M))%M;
+
+    }
+    
+    public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        
-        int n=sc.nextInt();
-        int q=sc.nextInt();
-       
-        int arr[]=new int[n+1];
-        
-        for(int i=1;i<=n;i++) {
-        	arr[i]=sc.nextInt();
+        long t=sc.nextLong();
+        while(t--!=0) {
+        	long n=sc.nextLong();
+        	if(n==0){
+        	    System.out.println("0 0");
+        	    continue;
+        	}
+        	long M=1000000007;
+        	long result=modularExponentiation(2,n-1,M);
+        	System.out.println((result-1)+" "+(result));
         }
-		while(q--!=0) {
-			int l=sc.nextInt();
-			int r=sc.nextInt();
-			int diff=sc.nextInt();
-			
-			HashMap<Integer,Integer> map=new HashMap<Integer, Integer>();
-			
-			for(int i=l;i<=r;i++) {
-			    if(!map.containsKey(arr[i])){
-					map.put(arr[i],arr[i]);
-				}
-				
-			}
-			
-			for(int i=l;i<=r;i++) {
-				if(map.containsKey(arr[i]-diff)){
-					map.put(arr[i],(arr[i]-diff));
-				}
-			}
-			int max=1;
-			HashMap<Integer,Integer> map_2=new HashMap<Integer, Integer>();
-			for(int i=l;i<=r;i++) {
-				if((map.get(arr[i]))==arr[i]){
-					map_2.put(arr[i],1);
-				
-				}
-				else {
-					int parent=getparent(arr[i],map);
-					if(map_2.containsKey(parent)) {
-						int val=map_2.get(parent);
-						val=val+1;
-						map_2.put(parent,val);
-						if(val>max) {
-							max=val;
-						}
-					}
-					else {
-						map_2.put(parent,1);
-						
-					}
-				}
-			}
-			
-		System.out.println(max);
-		}
-		
 	
 	
 	
